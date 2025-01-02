3cf5da72c8308890dfdd1f1e6cb8e9ca52a19ac9
keisuke.ueda <ukei2021@gmail.com>
2025-01-02 10:56:27 +0900

    init

---
+++ resources/js/Pages/SamplePage.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container mt-5">
+        <h1 class="text-primary">コンテナ php8.4+nginx/node(vite)/mysql</h1>
+        <h1 class="text-primary">アプリケーション Laravel11/Inertia/Vue3/TypeScript/Bootstrap5</h1>
+        <button class="btn btn-success" @click="checkType">Click Me</button>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+    name: 'SamplePage',
+    props: {
+        message: {
+            type: String as PropType<string>,
+            required: true,
+        },
+    },
+    methods: {
+        checkType() {
+            const value: number = 1;
+            console.log(value);
+            const unusedVar = 'This is not used';
+            //console.log(unusedVar);
+            
+
+        },
+    },
+});
+</script>
+
+
+
+<style scoped>
+
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import checker from 'vite-plugin-checker';
+import laravel from 'laravel-vite-plugin';
+import eslint from 'vite-plugin-eslint'; 
+
+export default defineConfig({
+    logLevel: 'error',
+    plugins: [
+        vue(),
+        checker({
+            vueTsc: true,
+            overlay: true,
+        }),
+        eslint({
+            emitWarning: true,
+            emitError: true,
+            failOnError: false,
+            failOnWarning: false,
+            fix: false,
+        }),
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/css/app.css',
+                'resources/js/app.ts',
+            ],
+            refresh: true,
+        }),
+    ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        watch: {
+            usePolling: true,
+        },
+        hmr: {
+            overlay: true,
+        },
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'), // エイリアス設定
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        },
+    },
+});
