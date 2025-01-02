2884c053708b5d32cbb1f9013191d19542b0b428
keisuke.ueda <ukei2021@gmail.com>
2025-01-02 14:49:34 +0900

    test

--- resources/js/Pages/SamplePage.vue
+++ resources/js/Pages/SamplePage.vue
@@ -18,7 +18,7 @@ export default defineComponent({
     },
     methods: {
         checkType() {
-            const value: number = 123;
+            const value: number = 1;
             console.log(value);
             const unusedVar = 'This is not used';
             console.log(unusedVar);
